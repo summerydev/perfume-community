ce3478deefcf9422d84a8feb45cdc9916512dd59
summer <summery.dev@gmail.com>
2022-11-09 14:13:07 +0900

    [ui] 향수 전체 조회 수정, genderMessage 추가

--- ui/src/views/Reviews.vue
+++ ui/src/views/Reviews.vue
@@ -5,37 +5,32 @@
     <div>
       <li v-for="review in this.reviewList" :key="review.id">
         <div>{{ review.perfume_name }}</div>
-        <div>{{ review.brand }}</div>
+        <div>{{ review.name }}</div>
         <div>{{ review.recommendation == "1" ? "추천해요" : "아쉬워요" }}</div>
-        <div class="reviewOptions">
-          <div>
-            지속력⏱️
-            <span v-if="review.longevity == 0">{{ longevityMessage[0] }}</span>
-            <span v-else-if="review.longevity == 1">{{
-              longevityMessage[1]
-            }}</span>
-            <span v-else>{{ longevityMessage[2] }}</span>
-          </div>
-          <div>확산력✨
-            <span v-if="review.strength == 0">{{ strengthMessage[0] }}</span>
-            <span v-else-if="review.longevity == 1">{{
-              strengthMessage[1]
-            }}</span>
-            <span v-else>{{ strengthMessage[2] }}</span>
-          </div>
-          <div>
-            for
-            <span v-for="item in review.gender" v-bind:key="item.gender"
-              >{{ item }}
-            </span>
-          </div>
-          <div>
-            <span
-              v-for="item in review.fragrance"
-              v-bind:key="item.fragrance"
-              >{{ item }}</span
-            >
-          </div>
+        <div>
+          지속력⏱️
+          <span v-if="review.longevity == 0">{{ longevityMessage[0] }}</span>
+          <span v-else-if="review.longevity == 1">{{
+            longevityMessage[1]
+          }}</span>
+          <span v-else>{{ longevityMessage[2] }}</span>
+        </div>
+        <div>
+          확산력✨
+          <span v-if="review.strength == 0">{{ strengthMessage[0] }}</span>
+          <span v-else-if="review.longevity == 1">{{
+            strengthMessage[1]
+          }}</span>
+          <span v-else>{{ strengthMessage[2] }}</span>
+        </div>
+        <div>
+          for
+          <span v-if="review.gender == 0">{{ genderMessage[0] }} </span>
+        </div>
+        <div>
+          <span v-for="item in review.fragrance" v-bind:key="item.fragrance">{{
+            item
+          }}</span>
         </div>
         <div>{{ review.content }}</div>
       </li>
@@ -49,7 +44,8 @@ export default {
   data() {
     return {
       longevityMessage: ["짧아요", "괜찮아요", "오래가요"],
-      strengthMessage: ["나만 맡을 수 있어요", "지하철 가능!", "강력해요F"]
+      strengthMessage: ["나만 맡을 수 있어요", "지하철 가능!", "강력해요"],
+      genderMessage: ["둘 다", "남성", "여성"],
     };
   },
   computed: {
@@ -68,9 +64,5 @@ li {
   padding: 10px;
   background-color: white;
   border-radius: 15px;
-}
-
-.reviewOptions {
-  display: flex;
 }
 </style>
