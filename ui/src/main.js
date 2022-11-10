a704729255e19065acb0ef1f143a375b250c70de
summer <summery.dev@gmail.com>
2022-11-10 15:46:43 +0900

    [ui] element ui 설치

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,14 +3,17 @@ import App from "./App.vue";
 import router from "./routes/routes";
 import axios from "axios";
 import { store } from "./store/index";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.prototype.$axios = axios;
 
-axios.defaults.baseURL = 'http://localhost:3000';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 
 Vue.config.productionTip = false;
+Vue.use(Element);
 
 new Vue({
   store,
