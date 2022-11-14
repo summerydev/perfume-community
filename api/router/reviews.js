168e434699e4ca0f14c4668b1d1a570d48990ad2
summer <summery.dev@gmail.com>
2022-11-14 17:57:16 +0900

    [api] 리뷰 수정 api 추가

--- api/router/reviews.js
+++ api/router/reviews.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /** DB pool */
 const pool = require("../db/db.js");
 
-/** [리뷰] 전체 리뷰 리스트 조회 */
+/** 전체 리뷰 리스트 조회 */
 router.get("/", async (req, res) => {
   const getReviewQuery =
     "select r.id, r.user_id, r.recommendation, r.longevity, r.strength, r.gender, r.fragrance, r.content, r.created_date, p.perfume_name, p.image_name, p.path, b.name, u.name as user_name from review r, perfume p, brand b, user u where r.perfume_id=p.id and p.brand_id=b.id and r.user_id=u.id order by r.created_date desc";
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-/** [리뷰] 리뷰 등록 */
+/** 리뷰 등록 */
 router.post("/", async (req, res) => {
   // console.log(req.body); => undefined
   const insertReviewQuery = `insert into review (user_id, perfume_id, recommendation, longevity, strength, gender, fragrance, content, created_date, modified_date) values(?,?,?,?,?,?,?,?, now(), now())`;
@@ -39,14 +39,23 @@ router.post("/", async (req, res) => {
   }
 });
 
-/** 향수 검색 */
-router.get("/perfume", async (req, res) => {
-  const searchKey = req.query.searchKey;
-  const getPerfumeIdQuery =
-    "select id, perfume_name from perfume where perfume_name like ?";
+/** 리뷰 수정 */
+router.put("/:id", async (req, res) => {
+  const reviewPkId = req.params.id;
+  const updateReviewQuery =
+    "update review set recommendation=?, longevity=?, strength=?, gender=?, fragrance=?, content=?, modified_date=now() where id=?";
   try {
-    const [rows] = await pool.query(getPerfumeIdQuery, `%${searchKey}%`);
-    res.json(rows);
+    await pool.query(updateReviewQuery, [
+      req.body.recommendation,
+      req.body.longevity,
+      req.body.strength,
+      req.body.gender,
+      req.body.fragrance,
+      req.body.content,
+      reviewPkId,
+    ]);
+
+    res.status(200).send({ result: "success" });
   } catch (e) {
     console.log(e);
     res.status(500).send({ result: "fail", message: e });
