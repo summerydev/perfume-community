ae18f4828fe03c2014d6fa04408f3cabbbb31fb7
summer <summery.dev@gmail.com>
2022-11-14 10:31:54 +0900

    [ui] 로그인 체크 로직 수정중

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -16,9 +16,29 @@ import AppFooter from "./components/AppFooter.vue";
 import AppHeader from "./components/AppHeader.vue";
 import NavBar from "./components/NavBar.vue";
 import Reviews from "./views/Reviews.vue";
+import axios from "axios";
 
 export default {
   components: { AppHeader, AppFooter, NavBar, Reviews },
+  mounted() {
+    async function loginCheck() {
+      const data = await axios.get("/", localStorage.accessToken);
+      if (data.status == 200) {
+        const result = await axios.put(`/users/${localStorage.userid}`);
+        console.log(result.data);
+        const accessToken = localStorage.getItem("accessToken");
+        const refreshToken = localStorage.getItem("refreshToken");
+        console.log("oo");
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(this.$router);
+        console.log(this.$store);
+        this.$store.commit("loginToken", [accessToken, refreshToken]);
+        this.$store.commit("loginSuccess", result.data);
+      }
+    }
+    loginCheck();
+  },
 };
 </script>
 
@@ -33,8 +53,7 @@ a:link {
   text-decoration: none;
 }
 a:visited {
-  color:#409eff;
+  color: #409eff;
   text-decoration: none;
 }
-
 </style>
