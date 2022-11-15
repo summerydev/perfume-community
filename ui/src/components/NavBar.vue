626ae72f899bfac6daefa1016c8589a62010717f
summer <summery.dev@gmail.com>
2022-11-15 17:17:02 +0900

    [ui] 로그아웃 라우터 새로고침 오류 수정

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -2,12 +2,15 @@
   <nav class="navbar">
     <el-row>
       <div v-if="this.isLogin">
-        <router-link to="/mypage"
-          ><el-button>마이페이지</el-button></router-link
-        >
+        <router-link to="/mypage">
+          <el-link>마이페이지</el-link>
+        </router-link>
+        <span> | </span>
+        <el-link @click="handleLogout">로그아웃</el-link>
+       
       </div>
       <div v-else>
-        <router-link to="/signin"><el-button>로그인</el-button></router-link>
+        <router-link to="/signin"><el-link>로그인</el-link></router-link>
       </div>
     </el-row>
   </nav>
@@ -22,7 +25,14 @@ export default {
   computed: {
     ...mapGetters({ isLogin: "getIsLogin" }),
   },
-  
+  methods: {
+    async handleLogout() {
+      this.$store.commit("logout");
+      alert("로그아웃 되었습니다.");
+      this.$router.go("/");
+      localStorage.clear();
+    },
+  },
 };
 </script>
 
@@ -30,4 +40,5 @@ export default {
 nav {
   float: right;
 }
+
 </style>
