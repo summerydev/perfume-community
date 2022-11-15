644dc5b87d4f1333e37c76cb39b120b1b64cafdf
summer <summery.dev@gmail.com>
2022-11-15 17:16:21 +0900

    [ui] 리뷰 작성하기 버튼 컴포넌트화

---
+++ ui/src/components/AddReviewBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link to="/addreview" class="addBtn">
+    <el-button round>
+      <i class="el-icon-edit"></i>
+      리뷰 작성하기
+    </el-button>
+  </router-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
