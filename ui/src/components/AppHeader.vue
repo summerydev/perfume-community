f272d9a81ce1556708576592d9b755e4d8f2083b
summer <summery.dev@gmail.com>
2022-11-21 17:26:11 +0900

    [ui] 향수별 통계 기능 view 추가

--- ui/src/components/AppHeader.vue
+++ ui/src/components/AppHeader.vue
@@ -2,12 +2,28 @@
   <header>
     <h1>
       <router-link to="/"><img src="../assets/logo.png" /></router-link>
+      <div class="nav">
+        <el-link @click="handleClick('/allreviews')" class="clickedLink">
+          전체리뷰보기
+        </el-link>
+        <el-link @click="handleClick('/perfumes')" class="clickedLink">
+          향수별리뷰보기
+        </el-link>
+      </div>
     </h1>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handleClick(link) {
+      this.$route.path == link
+        ? this.$router.go("")
+        : this.$router.push(`${link}`);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -15,5 +31,11 @@ img {
   width: 200px;
   height: 100px;
   object-fit: cover;
+}
+.nav {
+  text-align: center;
+}
+.el-link {
+  padding: 5px;
 }
 </style>
