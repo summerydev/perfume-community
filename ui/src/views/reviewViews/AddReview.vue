1146866aee6eeb305cf15e40b923aab9395480f5
summer <summery.dev@gmail.com>
2022-11-22 17:41:51 +0900

    [ui] 향수 검색 기능 uri 수정

--- ui/src/views/reviewViews/AddReview.vue
+++ ui/src/views/reviewViews/AddReview.vue
@@ -158,9 +158,7 @@ export default {
         this.isShow = true;
         this.searchResult = [];
         try {
-          const result = await this.$axios.get("/perfumes", {
-            params: { searchKey: this.searchKey },
-          });
+          const result = await this.$axios.get(`/perfumes/${this.searchKey}`);
           if (result.data.length > 0) {
             this.searchResult = result.data;
             console.log(result.data);
